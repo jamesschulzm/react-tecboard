12a0fef27fa3a455c05c77aa558072a5e5bdd138
James Schulz <jamesschulzm@gmail.com>
2025-12-29 18:46:22 -0300

    [ADD] Passagem de parâmetros

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 import "./App.css";
 
+// no react, componentes são FUNÇÕES
+
+// props é um OBJETO
+// props.children
+function TituloFormulario(props) {
+  return <h2>{props.children}</h2>;
+}
+
+function FormularioDeEvento() {
+  return (
+    <form className="form-evento">
+      <TituloFormulario>Preencha para criar um evento:</TituloFormulario>
+
+      <fieldset>
+        <label htmlFor="nome">Qual o nome do evento?</label>
+        <input type="text" id="nome" placeholder="Summer dev hits" />
+      </fieldset>
+    </form>
+  );
+}
+
 function App() {
   return (
     <main>
@@ -10,6 +31,8 @@ function App() {
       <section>
         <img src="/banner.png" alt="" />
       </section>
+
+      <FormularioDeEvento />
     </main>
   );
 }
