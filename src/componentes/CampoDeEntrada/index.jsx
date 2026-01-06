b590096447cd1069f6e1a5d79f1595863f297570
James Schulz <jamesschulzm@gmail.com>
2026-01-06 19:00:49 -0300

    [ADD] Estilizando componentes

--- src/componentes/CampoDeEntrada/index.jsx
+++ src/componentes/CampoDeEntrada/index.jsx
@@ -1,4 +1,6 @@
+import "./campo-de-entrada.estilos.css";
+
 // Espalhar as propiedades
 export function CampoDeEntrada(props) {
-  return <input {...props} />;
+  return <input {...props} className="campo-entrada-form" />;
 }
--- src/componentes/TituloFormulario/index.jsx
+++ src/componentes/TituloFormulario/index.jsx
@@ -1,5 +1,7 @@
+import "./titulo-formulario.estilos.css";
+
 // props é um OBJETO
 // props.children
 export function TituloFormulario({ children }) {
-  return <h2>{children}</h2>;
+  return <h2 className="titulo-form">{children}</h2>;
 }
