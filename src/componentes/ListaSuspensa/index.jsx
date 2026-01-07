145d008215dfceaa6dccc4a6889589990cd793c3
James Schulz <jamesschulzm@gmail.com>
2026-01-07 18:55:20 -0300

    [ADD] Lista suspensa e botões

---
+++ src/componentes/ListaSuspensa/index.jsx
@@ -0,0 +1,9 @@
+import "./lista-suspensa.estilos.css";
+
+export function ListaSuspensa() {
+  return (
+    <select className="lista-suspensa-form">
+      <option value="">Selecione uma opção</option>
+    </select>
+  );
+}
